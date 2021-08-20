73defd3297ab3453b1c85e8170c30d1963a1bca3
Daniela Küster <daniela.kuester@gmx.net>
2021-08-20 14:09:31 +0200

    Add modal with button

--- js/app.js
+++ js/app.js
@@ -163,6 +163,29 @@ function startTimer() {
     timer;
 }
 
+/* The code for the modal is from a W3 schools tutorial and adapted to this project https://www.w3schools.com/howto/howto_css_modals.asp */
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 // To stop the timer: clearInterval(timer);
 
 function winGame() {
